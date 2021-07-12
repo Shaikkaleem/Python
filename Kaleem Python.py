4dc48adfdbf469bed2a98e5c5ee8925cfd2513e2
Shaikkaleem <86420950+Shaikkaleem@users.noreply.github.com>
2021-07-12 14:19:51 +0530

    Add files via upload

---
+++ Kaleem Python.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Task 1
+
+# In[8]:
+
+
+a=[3,5,11,45,76,32,2]
+
+
+# In[9]:
+
+
+print(max(a))
+
+
+# In[10]:
+
+
+print(min(a))
+
+
+# In[16]:
+
+
+print(bool(a))
+
+
+# Task 2
+
+# In[24]:
+
+
+str1= "welcome to python"
+if "to" in str1:
+  print("true")
+else:
+  print("flase")
+
+
+
+
+
+
+
+
+
+
+
+
